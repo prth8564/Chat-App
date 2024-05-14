ad79b4ca3eebbeccc47a45f3876e8e92be8b7cff
prth8564 <preetham10107@gmail.com>
2024-05-14 22:52:41 +0530

    socket functionality,notif sounds

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,10 @@ import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 
-
+import { app , server } from "./socket/socket.js";
 import  connectToMongoDB  from "./db/connectToMongoDB.js"
 
-const app = express();
+
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
@@ -20,7 +20,7 @@ app.use("/api/messages" , messageRoutes);
 app.use("/api/auth" , authRoutes);
 app.use("/api/users" , userRoutes);
 
-app.listen(PORT , ()=>{ 
+server.listen(PORT , ()=>{ 
     connectToMongoDB();
     console.log(`Listening at ${PORT}`)
 });
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -2,11 +2,11 @@ import { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../Skeletons/MessageSkeleton";
 import Message from "./Message";
-//import useListenMessages from "../../hooks/useListenMessages";
+import useListenMessages from "../../hooks/useListenMessages";
 
 const Messages = () => {
 	const { messages, loading } = useGetMessages();
-	//useListenMessages();
+	useListenMessages();
 	console.log(messages.length);
 	const lastMessageRef = useRef();
 
